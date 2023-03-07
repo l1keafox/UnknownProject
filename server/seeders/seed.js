b21fd3e1d4d673396350f1e5d16203290827b5e4
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-03-07 09:21:36 -0700

    first commit

---
+++ server/seeders/seed.js
@@ -0,0 +1,17 @@
+const db = require("../config/connection");
+const { User } = require("../models");
+const userSeeds = require("./userSeeds.json");
+
+db.once("open", async () => {
+	try {
+		await User.deleteMany({});
+		await User.create(userSeeds);
+
+		let users = await User.find();
+		console.log(users);
+		console.log("all done!");
+		process.exit(0);
+	} catch (err) {
+		throw err;
+	}
+});
